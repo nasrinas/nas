87b87643dbd7dea430889462785e61018f9f64cc
Nasrin Sanati <nasrin@Nasrins-MacBook-Pro.local>
2019-04-22 20:17:01 -0700

    extend1 machin

--- src/CESKSTAR.hs
+++ src/CESKSTAR.hs
@@ -2,41 +2,56 @@ module CESKSTAR where
 
 import qualified Data.Map as M
 
-
+type Address = Int
 type Var = String
-
-data Lambd =  LE Var Expr deriving Show
-
-data Expr = Ref Var
-          | App Expr Expr
-          | Abs Lambd
-        deriving (Show)
-
+data Opr = Add | Sub | Mul | Leq deriving (Eq, Show)
+data Lambd =  Bind Var Expr deriving Show
 type Sigma = (Expr,Env,Store,Kont)
 type Env = M.Map Var Address
+
+-- The store now maps to denotable values and continuations:
+type Store = M.Map Address Storable
+
+data Expr = Ref Var
+          | Abs Lambd
+          | App Expr Expr
+          | BinOpr Opr Expr Expr
+          | If Expr Expr Expr
+          | LInt Int
+          | LBool Bool
+        deriving Show
+
 
  -- The Kont component of the machine is replaced by a pointer to a continuation
  -- allocated in the store.
 data Kont  = Mt
            | AppL Expr Env Address
            | AppR Lambd Env Address
-         deriving (Show)
+           | BinOprL Opr Expr Env Address
+           | BinOprR Opr Expr Env Address
+           | IfK Expr Expr Env Address
+         deriving Show
 
 
-data Storable = Clo Lambd Env
+data Storable = VInt Int
+              | VBool Bool
+              | Clo Lambd Env
               | Continue Kont
             deriving (Show)
 
--- The store now maps to denotable values and continuations:
-type Store = M.Map Address Storable
-
-type Address = Int
 
 ceskS :: Sigma -> Sigma
 ceskS (Ref x,p,s,k)
-  = (Abs lam,p',s,k)
+  = case s M.! (p M.! x) of
+      VInt i     -> (LInt i,p,s,k)
+      VBool b    -> (LBool b,p,s,k)
+      Clo lam p' -> (Abs lam,p',s,k)
+      _          -> error ("unknown")
+
+ceskS (If f t e,p,s,k)
+  = (f,p,M.insert addr' (Continue k) s,IfK t e p addr')
     where
-      Clo lam p' = s M.! (p M.! x)
+      addr' = label s
 
 ceskS ((App e1 e2),p,s,k)
   = (e1,p,M.insert addr' (Continue k) s ,AppL e2 p addr')
@@ -46,11 +61,18 @@ ceskS ((App e1 e2),p,s,k)
 ceskS ((Abs l),p,s,AppL e p' addr')
   = (e,p',s,AppR l p addr')
 
-ceskS ((Abs l),p,s,AppR (LE x e) p' a)
+ceskS ((Abs l),p,s,AppR (Bind x e) p' a)
   = (e,M.insert x addr' p',M.insert addr' (Clo l p) s,k)
     where
       addr'      = label s
       Continue k = s M.! a
+
+ceskS (ite,p,s,IfK t e p' addr')
+  = case ite of
+      LBool True  -> (t,p',s,k)
+      LBool False -> (e,p',s,k)
+  where
+    Continue k = s M.! addr'
 
 label :: Store -> Address
 label s = (foldl max 0 (M.keys s)) + 1
@@ -73,12 +95,10 @@ doAll :: Expr -> Sigma
 doAll e = reduction ceskS final (initial e)
 
 -- |  >>> doAll ex
---  (Abs (LE "y" (Ref "y")),fromList [],fromList [(1,Continue Mt),(2,Clo (LE "y" (Ref "y"),fromList []))],Mt)
+--  (Abs (Bind "y" (Ref "y")),fromList [],fromList [(1,Continue Mt),(2,Clo (Bind "y" (Ref "y"),fromList []))],Mt)
 ex :: Expr
 ex = App (abs' "x" (Ref "x")) (abs' "y" (Ref "y"))
 
 -- Smart constructor
 abs' :: Var -> Expr -> Expr
-abs' x e = Abs (LE x e)
-
--- tttttttttt
+abs' x e = Abs (Bind x e)
