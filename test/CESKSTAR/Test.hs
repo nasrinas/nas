55c8d3d558c3a0f575347e953829539d27fa297f
Nasrin Sanati <nasrin@10-249-147-178.wireless.oregonstate.edu>
2019-04-26 11:27:03 -0700

    new

--- test/CESKSTAR/Test.hs
+++ test/CESKSTAR/Test.hs
@@ -7,15 +7,18 @@ import Test.Tasty.HUnit
 
 import Data.List
 import Data.Ord
+import Data.Map
+
+import CESKSTAR
 
 
 tests :: TestTree
-tests = testGroup "Tests" [properties, unitTests]
+tests = testGroup "Tests" [unitTests]
 
-properties :: TestTree
-properties = testGroup "Properties" [scProps, qcProps]
+--properties :: TestTree
+--properties = testGroup "Properties" [scProps, qcProps]
 
-scProps = testGroup "(checked by SmallCheck)"
+{-scProps = testGroup "(checked by SmallCheck)"
   [ SC.testProperty "sort == sort . reverse" $
       \list -> sort (list :: [Int]) == sort (reverse list)
   , SC.testProperty "Fermat's little theorem" $
@@ -36,12 +39,35 @@ qcProps = testGroup "(checked by QuickCheck)"
       \x y z n ->
         (n :: Integer) >= 3 QC.==> x^n + y^n /= (z^n :: Integer)
   ]
-
+-}
 unitTests = testGroup "Unit tests"
-  [ testCase "List comparison (different length)" $
-      [1, 2, 3] `compare` [1,2] @?= GT
+  [ --testCase "List comparison (different length)" $
+--      [1, 2, 3] `compare` [1,2] @?= GT
 
   -- the following test does not hold
-  , testCase "List comparison (same length)" $
-      [1, 2, 3] `compare` [1,2,2] @?= LT
+--  , testCase "List comparison (same length)" $
+--      [1, 2, 3] `compare` [1,2,2] @?= LT
+--  , testCase "An example" myTest
+  testCase "basic application" initial'
+
   ]
+
+initial' :: Assertion
+initial' = myTestGen exT exResult
+
+myTestGen :: Expr -> Sigma -> Assertion
+myTestGen e s = result @?= s
+  where result = doAll e
+
+exT :: Expr
+exT = App (abs' "x" (Ref "x")) (abs' "y" (Ref "y"))
+
+exId :: Expr
+exId = App (abs' "x" (Ref "x")) (LInt 2)
+
+exResult :: Sigma
+exResult = ( Abs (Bind "y" (Ref "y"))
+            , empty
+            , fromList [(1,Continue Mt),(2,Clo (Bind "y" (Ref "y")) empty)]
+            , Mt
+            )
